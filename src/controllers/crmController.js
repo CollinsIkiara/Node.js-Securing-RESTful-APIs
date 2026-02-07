90c1434fe390ed86d744de1730cd8fb6fc2b6e8b
Collins Ikiara <cikiara@sowphie.farm>
2026-02-07 21:31:25 +0300

    Finalize setup for the project

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -3,49 +3,58 @@ import { ContactSchema } from '../models/crmModel';
 
 const Contact = mongoose.model('Contact', ContactSchema);
 
-export const addNewContact = (req, res) => {
-    let newContact = new Contact(req.body);
-
-    newContact.save((err, contact) => {
-        if (err) {
-            res.send(err);
-        }
+export const addNewContact = async (req, res) => {
+    try {
+        let newContact = new Contact(req.body);
+        const contact = await newContact.save();
         res.json(contact);
-    });
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
-export const getContacts = (req, res) => {
-    Contact.find({}, (err, contact) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(contact);
-    });
+export const getContacts = async (req, res) => {
+    try {
+        const contacts = await Contact.find({});
+        res.json(contacts);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
-export const getContactWithID = (req, res) => {
-    Contact.findById(req.params.contactId, (err, contact) => {
-        if (err) {
-            res.send(err);
+export const getContactWithID = async (req, res) => {
+    try {
+        const contact = await Contact.findById(req.params.contactId);
+        if (!contact) {
+            return res.status(404).send({ message: 'Contact not found' });
         }
         res.json(contact);
-    });
-}
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
 
-export const updateContact = (req, res) => {
-    Contact.findOneAndUpdate({ _id: req.params.contactId}, req.body, { new: true }, (err, contact) => {
-        if (err) {
-            res.send(err);
+export const updateContact = async (req, res) => {
+    try {
+        const contact = await Contact.findOneAndUpdate(
+            { _id: req.params.contactId }, 
+            req.body, 
+            { new: true }
+        );
+        if (!contact) {
+            return res.status(404).send({ message: 'Contact not found' });
         }
         res.json(contact);
-    })
-}
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
 
-export const deleteContact = (req, res) => {
-    Contact.remove({ _id: req.params.contactId }, (err, contact) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({ message: 'Successfully deleted contact'});
-    })
-}
+export const deleteContact = async (req, res) => {
+    try {
+        const contact = await Contact.deleteOne({ _id: req.params.contactId });
+        res.json({ message: 'Successfully deleted contact' });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
